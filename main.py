84d33a90979636c3cea3d375f1e704fc65b2aef9
Glaucio Padilha <glauciofpp@gmail.com>
2025-11-03 22:52:12 -0300

    chore: Adicionado interação com os arquivos da pasta de faturas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from Modulos import extract_text_from_pdf
 from Modulos import logger
 
@@ -5,7 +6,20 @@ from config import FATURAS_FOLDER
 
 logger.info("Iniciando o processamento de faturas.")
 
-file = fr"{FATURAS_FOLDER}\fatura_1762020700430.pdf"
-logger.debug(f"Processando o arquivo: {file}")
+def get_faturas_from_folder(folder_path: str):
+    try:
+        faturas = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.lower().endswith('.pdf')]
+        logger.info(f"Encontradas {len(faturas)} faturas na pasta: {folder_path}")
+        return faturas
+    except Exception as e:
+        logger.error(f"Erro ao listar faturas na pasta {folder_path}: {e}")
+        return []
+    
+def start_processing():
+    faturas = get_faturas_from_folder(FATURAS_FOLDER)
+    for fatura in faturas:
+        logger.debug(f"Processando o arquivo: {fatura}")
+        print(extract_text_from_pdf(fatura))
+    
 
-extract_text_from_pdf(file)
+start_processing()
