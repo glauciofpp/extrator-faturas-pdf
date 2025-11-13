21b6ed25a23d62c61eea90b0bb226120f918da14
Glaucio Padilha <glauciofpp@gmail.com>
2025-11-12 22:29:49 -0300

    feat: adicionado funçao para coletar dias faturados, consumo, numerdo do medidor e o valores de cosip, pis, confins e icms

--- Modulos/text_processing.py
+++ Modulos/text_processing.py
@@ -28,6 +28,13 @@ def get_data(text: str, concessionaria: str) -> dict:
         data["valor_total"] = extract_valor_total(text, patterns)
         data["cod_cli"] = extract_cod_cli(text, patterns)
         data["endereco"] = extract_endereco(text, patterns)
+        data["dias_fat"] = extract_dia_fat(text, patterns)
+        data["consumo"] = extract_consumo(text, patterns)
+        data["medidor"] = extract_medidor(text, patterns)
+        data["cosip"] = extract_cosip(text, patterns)
+        data["pis"] = extract_pis(text, patterns)
+        data["cofins"] = extract_cofins(text, patterns)
+        data["icms"] = extract_icms(text, patterns)
 
         return data
     except Exception as e:
@@ -62,5 +69,41 @@ def extract_cod_cli(text: str, patterns: dict) -> str|None:
 def extract_endereco(text: str, patterns: dict) -> str|None:
     pattern_key = "endereco"
     logger.debug(f"Extraindo endereço usando o padrão: {patterns.get(pattern_key, '')}")
+    endereco = extract_multiple_value(text, patterns.get(pattern_key, ""))
+    return treat_endereco(endereco)
+
+def extract_dia_fat(text: str, patterns: dict) -> str|None:
+    pattern_key = "dias_fat"
+    logger.debug(f"Extraindo dias faturados usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_consumo(text: str, patterns: dict) -> str|None:
+    pattern_key = "consumo"
+    logger.debug(f"Extraindo consumo usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_medidor(text: str, patterns: dict) -> str|None:
+    pattern_key = "medidor"
+    logger.debug(f"Extraindo medidor usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_cosip(text: str, patterns: dict) -> str|None:
+    pattern_key = "cosip"
+    logger.debug(f"Extraindo valor de cosip usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_pis(text: str, patterns: dict) -> str|None:
+    pattern_key = "pis"
+    logger.debug(f"Extraindo valor do pis usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_cofins(text: str, patterns: dict) -> str|None:
+    pattern_key = "cofins"
+    logger.debug(f"Extraindo valor do cofins usando o padrão: {patterns.get(pattern_key, '')}")
+    return extract_single_value(text, patterns.get(pattern_key, ""))
+
+def extract_icms(text: str, patterns: dict) -> str|None:
+    pattern_key = "icms"
+    logger.debug(f"Extraindo valor do icms usando o padrão: {patterns.get(pattern_key, '')}")
     endereco = extract_multiple_value(text, patterns.get(pattern_key, ""))
     return treat_endereco(endereco)
